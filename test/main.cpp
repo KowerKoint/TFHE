f77735d1bc36d90911df60c98a808ec1a9492c0d
KowerKoint <kowerkoint@gmail.com>
2023-07-17 08:14:50 +0000

    add TLWE test

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+#include <cassert>
+#include <random>
+
+#include "tlwe.hpp"
+
+void test_TLWE() {
+    TFHE::TLWE tlwe;
+    std::mt19937 mt(0);
+    for (int i = 0; i < 100; i++) {
+        auto s = tlwe.generate_s();
+        bool msg = mt() & 1;
+        auto c = tlwe.encrypt_single_binary(msg, s);
+        assert(tlwe.decrypt_single_binary(c, s) == msg);
+    }
+}
+
+// TODO: Google Testとか使う
+// CMake Targetでテストできるようにしたい
+int main() { test_TLWE(); }
